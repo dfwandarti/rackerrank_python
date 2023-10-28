05f906a5f793b8551eac39512d205dd5ded0991b
dfwandarti <41955075+dfwandarti@users.noreply.github.com>
2023-10-27 21:36:10 -0300

    Create power_mod_power.py

---
+++ power_mod_power.py
@@ -0,0 +1,8 @@
+#https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    print(pow(a,b))
+    print(pow(a, b, m))
