52a889e2d5f085fea009eb5009af12575c73b654
dfwandarti <41955075+dfwandarti@users.noreply.github.com>
2023-10-29 19:44:49 -0300

    Create find_the_percentage.py

---
+++ find_the_percentage.py
@@ -0,0 +1,14 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    
+    marks = student_marks.get(query_name)
+    avg = sum(marks)/3
+    print(f'{avg:.2f}')
